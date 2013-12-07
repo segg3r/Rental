1f64001a3c1177975f951e06e853a9e1ea8dd6f3
segg3r <segg3r@gmail.com>
2013-12-07 21:23:57 +0300

    More tables implemented

--- src/by/gsu/segg3r/rental/ifaces/IFilterItemHolder.java
+++
@@ -1,9 +0,0 @@
-package by.gsu.segg3r.rental.ifaces;
-
-import by.gsu.segg3r.rental.exceptions.UiException;
-
-public interface IFilterItemHolder<T> extends IItemHolder<T> {
-
-	void filter(String filter) throws UiException;
-
-}
--- src/by/gsu/segg3r/rental/ifaces/IItemUiStrings.java
+++
@@ -1,9 +0,0 @@
-package by.gsu.segg3r.rental.ifaces;
-
-public interface IItemUiStrings<T> {
-
-	String getChangeItemHeader();
-	String getAddItemHeader();
-	String getFrameHeader();
-	
-}
